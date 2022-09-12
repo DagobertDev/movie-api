129bc59e021c03748a51d8a8235e9e2d7f35044b
David Mannes <git@davidmannes.de>
2022-09-12 21:55:18 +0200

    Allow anonymous access to movie endpoints

--- MovieApi/Controllers/MoviesController.cs
+++ MovieApi/Controllers/MoviesController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MovieApi.Model;
 using MovieApi.Services;
@@ -5,6 +6,7 @@ using MovieApi.Services;
 namespace MovieApi.Controllers;
 
 [ApiController]
+[AllowAnonymous]
 [Route("[controller]")]
 public class MoviesController : ControllerBase
 {
